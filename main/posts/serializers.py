a5e677c1c6d57a233c810d22df61d59ec51c9435
oliver <mail@mail.com>
2021-07-11 19:01:47 +0400

    #001 Implement basic features 1) DRF settings and `AUTH_USER_MODEL` have been added to main/settings.py. 2) API urls and `login` URL have been included into urlpatterns in */urls.py 3) Admin classes, Models, managers, serializers, urls and views have been created in according files in posts/ 4) .gitignore, Pipfile, Pipfile.lock, requirements.txt have been updated.

---
+++ main/posts/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+
+from .models import Post, User, Like
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password')
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'content',)
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    is_like = serializers.BooleanField(read_only=True)
+    post = serializers.CharField(read_only=True)
+    user = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ('id', 'is_like', 'post', 'user')
+
+    def create(self, validated_data):
+        instance, _ = Like.objects.update_or_create(
+            **validated_data, defaults={'is_like': True}
+        )
+        return instance
+
+
+class UnlikeSerializer(serializers.ModelSerializer):
+    is_like = serializers.BooleanField(read_only=True)
+    post = serializers.CharField(read_only=True)
+    user = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ('id', 'is_like', 'post', 'user')
+
+    def create(self, validated_data):
+        instance, _ = Like.objects.update_or_create(
+            **validated_data, defaults={'is_like': False}
+        )
+        return instance
